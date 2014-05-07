579c1a4025b2673f24de5083bb81d22e2285725e
andreas.ottesen <andreas.ottesen@acnestudios.com>
2014-05-07 16:58:33 +0200

    adding stuff

---
+++ app/src/main/java/se/andreasottesen/epguidesviewer/app/EpEntities/Show.java
@@ -0,0 +1,8 @@
+package se.andreasottesen.epguidesviewer.app.EpEntities;
+
+/**
+ * Created by andreas.ottesen on 2014-05-07.
+ */
+public class Show {
+    public String title;
+}
---
+++ app/src/main/java/se/andreasottesen/epguidesviewer/app/EpEntities/Shows.java
@@ -0,0 +1,10 @@
+package se.andreasottesen.epguidesviewer.app.EpEntities;
+
+import java.util.List;
+
+/**
+ * Created by andreas.ottesen on 2014-05-07.
+ */
+public class Shows {
+    public List<Show> shows;
+}
